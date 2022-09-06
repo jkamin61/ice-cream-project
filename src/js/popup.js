40d2d860a870b3d6af90b7063ac616e6fdaef651
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2022-09-06 15:47:22 +0200

    Create popup.js

---
+++ src/js/popup.js
@@ -0,0 +1,9 @@
+        let popup = document.getElementById("popup");
+
+        function openPopup() {
+            popup.classList.add("popup__btn");
+        }
+
+        function closePopup() {
+            popup.classList.remove("popup__btn");
+        }
