bae1106a02052c5a362fb8e8228be35fa73690ba
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2022-09-06 16:01:55 +0200

    Update popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,9 +1,9 @@
         let popup = document.getElementById("popup");
 
         function openPopup() {
-            popup.classList.add("popup__btn");
+            popup.classList.add("open-popup");
         }
 
         function closePopup() {
-            popup.classList.remove("popup__btn");
+            popup.classList.remove("open-popup");
         }
