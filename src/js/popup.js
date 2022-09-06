4e10cd7089719332af37b22e233c62ae969bd300
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2022-09-06 21:24:37 +0200

    Update popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -10,16 +10,19 @@
     // When the user clicks the button, open the modal 
     openPopup.onclick = function () {
         popup.style.display = "block";
+        popup.style.transform = "none";
     }
 
     // When the user clicks on <span> (x), close the modal
     closePopup.onclick = function () {
         popup.style.display = "none";
+        popup.style.transform = "translate(-50%, -50%) scale(0.1)";
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == popup) {
             popup.style.display = "none";
+            popup.style.transform = "translate(-50%, -50%) scale(0.1)";
         }
     }
