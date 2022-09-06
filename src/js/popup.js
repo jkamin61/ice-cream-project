1f21ac6e136f818f5f0f48a2008c9f53d60dead9
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2022-09-06 21:00:29 +0200

    Update popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,9 +1,25 @@
-        let popup = document.getElementById("popup");
+    // Get the modal
+    var popup = document.getElementById("popup");
 
-        function openPopup() {
-            popup.classList.add("open-popup");
-        }
+    // Get the button that opens the modal
+    var openPopup = document.getElementById("open-popup");
 
-        function closePopup() {
+    // Get the <span> element that closes the modal
+    var closePopup = document.getElementsByClassName("close-popup")[0];
+
+    // When the user clicks the button, open the modal 
+    openPopup.onclick = function () {
+        popup.classList.add("open-popup");
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    closePopup.onclick = function () {
+        popup.classList.remove("open-popup");
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == popup) {
             popup.classList.remove("open-popup");
         }
+    }
