8bf53b6193416ffacb45a8b17d6fd03135b2551a
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2022-09-06 09:21:13 +0200

    Update modal-buy.js

--- src/js/modal-buy.js
+++ src/js/modal-buy.js
@@ -1,14 +1,25 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+    // Get the modal
+    var modal = document.getElementById("modal-buy-now");
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
