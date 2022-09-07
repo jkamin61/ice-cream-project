0b7fffb10716e99cc09fb50a0daafa07667ea7ae
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2022-09-07 09:29:09 +0200

    Update popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,16 +5,24 @@
     var openPopup = document.getElementById("open-popup");
 
     // Get the button that opens the modal
-    var openPopup = document.getElementById("open-popup1");
+    var openPopup1 = document.getElementById("open-popup1");
 
     // Get the button that opens the modal
-    var openPopup = document.getElementById("open-popup2");
+    var openPopup2 = document.getElementById("open-popup2");
 
     // Get the <span> element that closes the modal
     var closePopup = document.getElementsByClassName("close-popup")[0];
 
     // When the user clicks the button, open the modal 
     openPopup.onclick = function () {
+        popup.style.transform = "none";
+    }
+    // When the user clicks the button, open the modal 
+    openPopup1.onclick = function () {
+        popup.style.transform = "none";
+    }
+    // When the user clicks the button, open the modal 
+    openPopup2.onclick = function () {
         popup.style.transform = "none";
     }
 
